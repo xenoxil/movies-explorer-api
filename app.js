d3185481f7927feb8ecbd7fcf8cdf3f7f54f3126
Xenoxil <xilus1127@gmail.com>
2022-08-19 14:26:44 +0300

    пофиксить корс

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 150, // limit each IP to 150 requests per windowMs
 });
-app.options('*', (req, res) => {
+/* app.options('*', (req, res) => {
   res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
   // res.set('Access-Control-Allow-Origin', 'https://xenoxil.movie-explorer.nomoreparties.sbs');
   res.set('Access-Control-Allow-Headers', 'Content-Type');
   res.set('Access-Control-Allow-Methods', ['PUT', 'GET', 'POST', 'DELETE', 'PATCH']);
   res.set('Access-Control-Allow-Credentials', 'true');
   res.send('ok');
-});
+}); */
 
 app.use(
   cors({
